7140b9c9b2af10fa16669f1f3b5439e9a466e5e4
Motts182 <mnd.matias.diaz@gmail.com>
2016-02-12 16:34:23 -0300

    V0.0.1 Colliders estructura nivel 1

---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+	public float movementSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * Time.deltaTime*movementSpeed);
+	}
+}
